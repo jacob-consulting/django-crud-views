2f7b7289142ffc1d3758d025369d488ebe924674
Alexander Jacob <alexander.jacob@jacob-consulting.de>
2025-05-01 07:21:29 +0200

    add TableWithViewContext

--- crud_views/lib/table/__init__.py
+++ crud_views/lib/table/__init__.py
@@ -1,9 +1,10 @@
-from .table import Table
+from .table import Table, TableWithViewContext
 from .columns import ActionColumn, LinkChildColumn, LinkDetailColumnMixin, LinkDetailColumn, UUIDColumn, \
     UUIDLinkDetailColumn
 
 __all__ = [
     "Table",
+    "TableWithViewContext",
     "ActionColumn",
     "LinkChildColumn",
     "LinkDetailColumnMixin",
--- crud_views/lib/table/table.py
+++ crud_views/lib/table/table.py
@@ -2,26 +2,35 @@ from typing import Type
 
 import django_tables2 as tables
 
-from .attrs import ColumnAttrs, ColAttr
+from .attrs import ColAttr
 from .columns import ActionColumn
 
 
-class Table(tables.Table):
+class TableWithViewContext(tables.Table):
     """
-    UI CRUD table with actions column
+    Table with view context
     """
-    actions = ActionColumn()
     ca: Type[ColAttr] = ColAttr
 
     def __init__(self, *args, **kwargs):
-        # kwargs["template_name"] = "django_tables2/bootstrap5.html"
-        if "sequence" not in kwargs:
-            kwargs["sequence"] = ("...", "actions")
         if not "view" in kwargs:
             raise ValueError(f"view not set in {self.__class__}")
         view = kwargs.pop("view")
         super().__init__(*args, **kwargs)
         self.view = view
+
+
+class Table(TableWithViewContext):
+    """
+    Table with action column
+    """
+    actions = ActionColumn()
+
+    def __init__(self, *args, **kwargs):
+        if "sequence" not in kwargs:
+            # as default make sure actions are at the end
+            kwargs["sequence"] = ("...", "actions")
+        super().__init__(*args, **kwargs)
 
     @staticmethod
     def order_actions(queryset, is_descending):

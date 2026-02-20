b091a18b20a10ea3bf384bbf4a95aa47dc249577
Alexander Jacob <alexander.jacob@jacob-consulting.de>
2026-02-20 17:55:27 +0100

    crud_views_workflow short label

--- crud_views_workflow/apps.py
+++ crud_views_workflow/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class CrudViewsWorkflowConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'crud_views_workflow'
+    label = "cvw"
 
     def ready(self):
         # todo: check if needed apps are installed: fsm2 fsm2-admin auditlog

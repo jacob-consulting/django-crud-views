b292bf345a5d1ba8956da1faaa1977414a63469b
Alexander Jacob <alexander.jacob@jacob-consulting.de>
2026-02-21 11:43:35 +0100

    Add Meta ordering to test models to fix UnorderedObjectListWarning

--- tests/test1/app/models.py
+++ tests/test1/app/models.py
@@ -27,6 +27,9 @@ class Author(OrderedModel):
 class Publisher(models.Model):
     name = models.CharField(max_length=200)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
 
@@ -34,6 +37,9 @@ class Publisher(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, related_name="books")
+
+    class Meta:
+        ordering = ["title"]
 
     def __str__(self):
         return self.title
@@ -106,6 +112,9 @@ class Campaign(WorkflowMixin, models.Model):
 
 class Vehicle(PolymorphicModel):
     name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ["name"]
 
     def __str__(self):
         return self.name

f2c3288a71a4a619684aaaa62911a0bd45377637
Alexander Jacob <alexander.jacob@jacob-consulting.de>
2026-04-01 18:31:20 +0200

    feat: register set_language URL for i18n language switching

--- examples/bootstrap5/project/urls.py
+++ examples/bootstrap5/project/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("app.urls")),
     path("admin/", admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
 ]

fe7919bac06a390bbdaa580e85221e1c25b588f0
Alexander Jacob <alexander.jacob@jacob-consulting.de>
2026-05-18 15:34:53 +0200

    fix: add cv_property_display to BookReviewDetailView and create action to card list

--- examples/bootstrap5/app/views/book_review.py
+++ examples/bootstrap5/app/views/book_review.py
@@ -43,6 +43,7 @@ class BookReviewCardListView(GuardianCardListViewPermissionRequired):
     cv_viewset = cv_book_review
     cv_path = ""
     cv_card_container_class = "col-md-12"
+    cv_context_actions = ["parent", "card", "filter", "create"]
     cv_card_actions = [
         CardAction(key="detail", label="Details", variant="primary", flex=True),
         CardAction(key="update", label="Edit"),
@@ -53,7 +54,17 @@ class BookReviewCardListView(GuardianCardListViewPermissionRequired):
 class BookReviewDetailView(GuardianDetailViewPermissionRequired):
     cv_viewset = cv_book_review
     cv_context_actions = ["card", "detail", "update", "delete"]
-
+    cv_property_display = [
+        {
+            "title": _("Review"),
+            "icon": "comment",
+            "properties": [
+                "reviewer",
+                "rating",
+                "text",
+            ],
+        },
+    ]
 
 class BookReviewCreateView(
     CrispyModelViewMixin, MessageMixin, CreateViewParentMixin, GuardianCreateViewPermissionRequired
